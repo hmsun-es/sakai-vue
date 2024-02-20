cdf91d6cbd964872887fb0d38c8e652c9b30d0f4
Hyeonmin Seon <hyunmui@outlook.kr>
2024-02-20 23:19:57 +0900

    convert typescript

--- src/layout/AppConfig.vue
+++ src/layout/AppConfig.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import RadioButton from 'primevue/radiobutton';
 import Button from 'primevue/button';
 import InputSwitch from 'primevue/inputswitch';
--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { useLayout } from '@/layout/composables/layout';
 import { ref, computed } from 'vue';
 import AppConfig from '@/layout/AppConfig.vue';
